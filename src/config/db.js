d0b5ab96b7c83a2b46291741b21b86ad124241ce
prasanthanupojucodeweb <anupojubhavani.codewebster@gmail.com>
2026-04-01 16:15:53 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI)
+    const conn = await mongoose.connect(process.env.mongodb+srv://anupojubhavanicodewebster_db_user:CDh7DqQEXYqMeRdq@cluster0.qd7zmyt.mongodb.net/?appName=Cluster0)
     console.log(`MongoDB connected: ${conn.connection.host}`)
   } catch (error) {
     console.error(`MongoDB connection error: ${error.message}`)
